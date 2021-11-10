0f5a1b351811e39da9cadc0bd3c2cf99b623d542
thlohith <thlohith@din84000612.corp.capgemini.com>
2021-11-10 14:10:03 +0530

    AGAIN ADDED

--- sampledemo/src/main/java/sampledemo/sampleclass.java
+++ sampledemo/src/main/java/sampledemo/sampleclass.java
@@ -3,7 +3,9 @@ package sampledemo;
 public class sampleclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hii");
+		
 
 	}
 
